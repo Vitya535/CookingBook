43e1583b33d8e4aaf22ba7e7a7e92cd23475d4a1
Vitya535 <kushnerenko.viktor@gmail.com>
2018-09-28 16:14:06 +0300

    Сделал ajax-запрос для изменения таблицы в зависимости от того, что нажато в списке, подсветка по нажатию для ячеек, отображение их текста в textarea, изменение текста в ячейках.

--- server.py
+++ server.py
@@ -3,14 +3,20 @@ from flask import Flask, render_template
 from flask_assets import Environment
 from flask_csp.csp import csp_header
 import sqlite_db
-# from utils import *
-# from datetime import datetime
 # from html import escape - для экранирования (защита от XSS)
 
 APP = Flask(__name__)
 APP.config.from_pyfile('config.py')
 
 ASSETS = Environment(APP)
+
+
+@APP.route('/<path:selected_option>')
+def show_select_content(selected_option):
+    """Показ содержания таблицы, которая выбрана из списка"""
+    return render_template('table_view.html', selected=selected_option, titles=sqlite_db.get_titles(),
+                           attrs=sqlite_db.get_titles_of_attrs(selected_option),
+                           data=sqlite_db.get_all_data_from_table(selected_option))
 
 
 @APP.route('/')
@@ -27,23 +33,8 @@ ASSETS = Environment(APP)
     "base-uri": "",
     "report-uri": "/csp_report"
 })
-def show_content():
-    """Показ содержания кулинарной книги"""
-    # sqlite_db.add_dish('Жареная курица', 'Это блюдо вызовет у вас восхищение!', 8, TypesOfDish.MEAT_DISHES.value)
-    # sqlite_db.add_dish('Суп', 'Этот суп просто прекрасен!', 10, TypesOfDish.LEAN_DISHES.value)
-    # sqlite_db.add_recipe('static/images/1.jpg', 'https://www.russianfood.com/recipes/bytype/?fid=1454',
-    # datetime(minute=20, year=DEFAULT_YEAR, month=DEFAULT_MONTH, day=DEFAULT_DAY), datetime(minute=40,
-    # year=DEFAULT_YEAR, month=DEFAULT_MONTH, day=DEFAULT_DAY), 'Жареная курица') sqlite_db.add_recipe(
-    # 'static/images/2.jpg', 'https://www.russianfood.com/recipes/bytype/?fid=11', datetime(minute=30,
-    # year=DEFAULT_YEAR, month=DEFAULT_MONTH, day=DEFAULT_DAY), datetime(minute=50, year=DEFAULT_YEAR,
-    # month=DEFAULT_MONTH, day=DEFAULT_DAY), 'Суп') sqlite_db.add_ingredient('Перец', 300,
-    # UnitsOfMeasurement.GRAM.value, 'Жареная курица') sqlite_db.add_ingredient('Свинина', 500,
-    # UnitsOfMeasurement.GRAM.value, 'Суп') sqlite_db.add_implement('Миска',
-    # 'https://www.russianfood.com/recipes/bytype/?fid=1454') sqlite_db.add_implement('Черпак',
-    # 'https://www.russianfood.com/recipes/bytype/?fid=11') sqlite_db.add_step_of_cook(1, 'ляляля',
-    # 'https://www.russianfood.com/recipes/bytype/?fid=1454') sqlite_db.add_step_of_cook(2, 'тттттт',
-    # 'https://www.russianfood.com/recipes/bytype/?fid=11')
-    # print(sqlite_db.get_all_data_from_table('dish'))
+def show_init_content():
+    """Показ содержания таблицы dish"""
     return render_template('table_view.html', titles=sqlite_db.get_titles(),
                            attrs=sqlite_db.get_titles_of_attrs('dish'),
                            data=sqlite_db.get_all_data_from_table('dish'))
---
+++ templates/table_actions.js
@@ -0,0 +1,49 @@
+$(document).ready(function()
+{
+    var green_td;
+    $('#apply').click(function()
+    {
+        var textarea_value = $('textarea.update').val();
+        $(green_td).text(textarea_value);
+    });
+
+    $('#delete').click(function()
+    {
+        alert($('row_for_items:first-child').text())
+    });
+
+    $('table.content').click(function(e)
+    {
+        var rows = $(this).find('td');
+        $.each(rows, function(index, value)
+        {
+            rows.eq(index).css("background","");
+        });
+        if (e.target.className == 'item')
+        {
+            $(e.target).css("background","green");
+            green_td = e.target;
+            $('textarea.update').empty();
+            $('textarea.update').append($(e.target).html());
+        }
+        else if (e.target.tagName == 'TH')
+        {
+            $('textarea.update').empty();
+            // можно отсортировать еще по тому атрибуту, который подсвечен
+        }
+    });
+
+    $('#what_show').change(function()
+    {
+        var selected = $('#what_show option:selected').text();
+        $.ajax
+        ({
+            type: "GET",
+            url: "/" + selected,
+            success: function()
+            {
+                window.location.href = "/" + selected;
+            }
+        })
+    });
+});
